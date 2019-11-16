611e01307a63259d8ee69414fab4645e103bb046
Andreas_Just <andreas.wq@gmail.com>
2019-11-16 11:10:16 +0200

    Updates

---
+++ precache-manifest.654cfebe0d2cc338e2ff70e7fa1ccf45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73969a227c4028d645aae8d8ae541ada",
+    "url": "/react-redux-blank/index.html"
+  },
+  {
+    "revision": "fc9c8a4da6793c03060b",
+    "url": "/react-redux-blank/static/css/main.be7937ad.chunk.css"
+  },
+  {
+    "revision": "98df98bc6d14424e10d1",
+    "url": "/react-redux-blank/static/js/2.417319de.chunk.js"
+  },
+  {
+    "revision": "fc9c8a4da6793c03060b",
+    "url": "/react-redux-blank/static/js/main.245dbe6d.chunk.js"
+  },
+  {
+    "revision": "cf59613e2ecb1cf88e45",
+    "url": "/react-redux-blank/static/js/runtime-main.4aff9625.js"
+  }
+]);
