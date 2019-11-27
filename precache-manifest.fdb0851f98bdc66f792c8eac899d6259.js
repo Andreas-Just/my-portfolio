bf3a1c710a2841c0e0d8ab28ca486bc8cbdcb855
Andreas_Just <andreas.wq@gmail.com>
2019-11-27 09:37:49 +0200

    Updates

--- precache-manifest.fdb0851f98bdc66f792c8eac899d6259.js
+++ precache-manifest.fdb0851f98bdc66f792c8eac899d6259.js
@@ -1,19 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a265f3eb48c3aab5570f19a588c1d31e",
+    "revision": "021ddd87ec0b6a45d2be4fe8dd2124dc",
     "url": "/my-portfolio/index.html"
-  },
-  {
-    "revision": "8a3149487d1b900da447",
-    "url": "/my-portfolio/static/css/main.24cb606e.chunk.css"
   },
   {
     "revision": "9dd465e052fea242dc28",
     "url": "/my-portfolio/static/js/2.6b9054c7.chunk.js"
   },
   {
-    "revision": "8a3149487d1b900da447",
-    "url": "/my-portfolio/static/js/main.b96ed963.chunk.js"
+    "revision": "ceb47f2615157dc15cd1",
+    "url": "/my-portfolio/static/js/main.b3e21dd5.chunk.js"
   },
   {
     "revision": "d85d27cac53a8e7a29fd",
